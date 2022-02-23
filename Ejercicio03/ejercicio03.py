a991e8fdac49ace407e72e502945ef3c5313d821
AlphyDev <rodrigojimenez@alphystica.com>
2022-02-23 15:11:04 -0400

    Ejercicio03 resuelto_

--- Ejercicio03/ejercicio03.py
+++ Ejercicio03/ejercicio03.py
@@ -1,7 +1,7 @@
 cadena = str(input('Ingrese una cadena de texto: '))
 vocales = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
-def eliminar_vocales(cadena, letras_para_eliminar):
+def eliminar_vocales(cadena: str, letras_para_eliminar: list):
     cadena_return = cadena
     for letra in letras_para_eliminar:
         cadena_return = cadena_return.replace(letra, "")
